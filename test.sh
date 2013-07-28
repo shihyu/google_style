35b03f142c28e035483cc10c6135a95fd726454b
jason_yao <jason_yao@htc.com>
2013-07-28 19:10:15 +0800

    wegwer

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-find . -type f \( -name "*.htm" -o -name "*.html" \)|while read line;do
-echo $line
-cconv -f utf-8 -t utf8-tw $line -o ${line}.zh_TW
-mv $line ${line}.zh_CN
-mv ${line}.zh_TW $line
-done
-find . -type f -name "*.zh_CN" -exec rm -f {} \;
